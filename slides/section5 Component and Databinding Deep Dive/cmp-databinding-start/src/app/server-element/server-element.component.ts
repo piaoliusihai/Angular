74908a065dc0d468bc8a6d5c2a9e0b334112dbb6
piaoliusihai <piaoliusihai@gmail.com>
2020-06-18 23:11:05 +0800

    section 5 79 Seeing Lifecycle Hooks in Action

--- slides/section5 Component and Databinding Deep Dive/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ slides/section5 Component and Databinding Deep Dive/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,14 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  ViewEncapsulation,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,13 +16,45 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('Constructor called!');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck(): void {
+    console.log('noDoCheck called!');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
+  }
 }
